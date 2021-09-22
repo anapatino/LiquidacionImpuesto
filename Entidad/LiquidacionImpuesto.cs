8694f4dace5d6d97071429306a244279c21f028a
anapatino <79620046+anapatino@users.noreply.github.com>
2021-09-22 13:03:28 -0500

    se confirma punto 2

---
+++ Entidad/LiquidacionImpuesto.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Entidad
+{
+    public class LiquidacionImpuesto
+    {
+        private long identificacion;
+        private double valorIngresoAnuales, valorGastosAnuales, UVT = 30.000,ganancia,tarifa,valorUVT,valorLiquidacion;
+        private int tiempoFuncionamiento;
+        private string tipoResponsabilidad,nombreEstableciemnto;
+
+        public LiquidacionImpuesto()
+        {
+
+        }
+
+        public LiquidacionImpuesto(long identificacion,string nombreEstablecimiento,double valorIngresoAnuales,double valorGastosAnuales ,string tipoResponsabilidad,int tiempoFuncionamiento)
+        {
+            Identificacion=identificacion;
+            NombreEstablecimiento=nombreEstablecimiento;
+            ValorIngresoAnual=valorIngresoAnuales;
+            ValorGastoAnual=valorGastosAnuales;
+            TipoResponsabilidad=tipoResponsabilidad;
+            TiempoFuncionamiento = tiempoFuncionamiento;
+            Ganancia = ganancia;
+            Tarifa = tarifa;
+            ValorUVT = valorUVT;
+            ValorLiquidacion = valorLiquidacion;
+        }
+        public long Identificacion { get; set; }
+        public string NombreEstablecimiento { get; set; }
+        public double ValorIngresoAnual { get; set; }
+        public double ValorGastoAnual { get; set; }
+        public string TipoResponsabilidad { get; set; }
+        public int TiempoFuncionamiento { get; set; }
+        public double Ganancia { get; set; }
+        public double Tarifa { get; set; }
+        public double ValorUVT { get; set; }
+        public double ValorLiquidacion { get; set; }
+
+        public void CalcularLiquidacion()
+        {
+            CalcularGanancia();
+            CalcularTarifa();
+            CalcularValorUVT();
+            ValorLiquidacion = Ganancia * (Tarifa / 100);
+        }
+
+        public void CalcularGanancia()
+        {
+            Ganancia = ValorIngresoAnual - ValorGastoAnual;
+        }
+        public void CalcularValorUVT()
+        {
+            ValorUVT=Ganancia / UVT;
+        }
+
+        public void CalcularTarifa()
+        {
+            if (TipoResponsabilidad.ToUpper().Equals("IVA"))
+            {
+                CalcularResponsableTarifa();
+            }
+            else 
+            {
+                CalcularNoResponsableTarifa();
+            }
+        }
+
+        public void CalcularResponsableTarifa()
+        {
+            if (Ganancia < 0)
+            {
+                Tarifa = 0;
+            }
+            else if (Ganancia < (UVT*100))
+            {
+                Tarifa = 5;
+            }
+            else if ((Ganancia >= (UVT * 100))&& (Ganancia <= (UVT * 200)))
+            {
+                Tarifa = 10;
+            }
+            else if (Ganancia <= (UVT * 500))
+            {
+                Tarifa = 15;
+            }
+
+        }
+        public void CalcularNoResponsableTarifa()
+        {
+            if (Ganancia > (UVT * 100))
+            {
+                if (TiempoFuncionamiento < 5)
+                {
+                    Tarifa = 1;
+                }
+                else if ((TiempoFuncionamiento >= 5)&& (TiempoFuncionamiento <10))
+                {
+                    Tarifa = 2;
+                }
+                else if (TiempoFuncionamiento >= 10)
+                {
+                    Tarifa = 3;
+                }
+            }
+            else
+            {
+                Tarifa = 0;
+            }
+           
+
+        }
+    }
+}
--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -8,7 +8,78 @@ namespace Presentacion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char seguir = 'S';
+            do
+            {
+                int opcion = Menu();
+                switch (opcion)
+                {
+                    case 1:
+                        RegistrarDatos();
+                        break;
+                    case 2:
+                        seguir = 'N';
+                        break;
+
+                }
+            } while (seguir == 'S');
+           
+
+        }
+        
+        public static int Menu()
+        {
+            int opcion;
+            Console.Clear();
+            Console.WriteLine("-----Liquidaciones de Impuestos---");
+            Console.WriteLine("1.Registrar Establecimiento");
+            Console.WriteLine("2.Salir");
+            Console.WriteLine();
+            Console.Write(  "Seleccione su opcion->");
+            do
+            {
+                opcion=int.Parse(Console.ReadLine());
+            } while (opcion < 1 && opcion > 2);
+
+            return opcion;
+        }
+
+        public static void RegistrarDatos()
+        {
+            long identificacion;
+            string nombreEstablecimiento,tipoResponsabilidad;
+            double valorIngresoAnual, valorGastosAnual;
+            int  tiempoFuncionamiento;
+            LiquidacionImpuesto persona;
+            Console.Clear();
+            Console.Write("Identificacion del Establecimiento :"); identificacion = long.Parse(Console.ReadLine());
+            Console.Write("Nombre del Establecimiento         :"); nombreEstablecimiento = Console.ReadLine();
+            Console.Write("Valor Ingreso Anual                :"); valorIngresoAnual = double.Parse(Console.ReadLine());
+            Console.Write("Valor Gasto   Anual                :"); valorGastosAnual = double.Parse(Console.ReadLine());
+            Console.Write("Tiempo de Funcionamiento           :"); tiempoFuncionamiento =int.Parse(Console.ReadLine());
+            Console.Write("Tipo de Responsabilidad            :"); tipoResponsabilidad = Console.ReadLine();
+
+            persona = new(identificacion, nombreEstablecimiento,  valorIngresoAnual, valorGastosAnual, tipoResponsabilidad,tiempoFuncionamiento);
+            persona.CalcularLiquidacion();
+            Consultar(persona);
+
+        }
+
+        public static void Consultar(LiquidacionImpuesto persona)
+        {
+            Console.Clear();
+            Console.WriteLine($"Identificacion del Establecimiento ({persona.Identificacion})");
+            Console.WriteLine($"Nombre del Establecimiento         ({persona.NombreEstablecimiento})"); 
+            Console.WriteLine($"Valor Ingreso Anual                 ({persona.ValorIngresoAnual})");
+            Console.WriteLine($"Valor Gasto   Anual                ({persona.ValorGastoAnual})"); 
+            Console.WriteLine($"Tiempo de Funcionamiento           ({persona.TiempoFuncionamiento})"); 
+            Console.WriteLine($"Tipo de Responsabilidad            ({persona.TipoResponsabilidad})");
+            Console.WriteLine("             -------------------------------");
+            Console.WriteLine($"Ganancia-> ({persona.Ganancia})");
+            Console.WriteLine($"Valor UTV-> ({persona.ValorUVT})");
+            Console.WriteLine($"Tarifa Aplicada-> ({persona.Tarifa})");
+            Console.WriteLine($"Valor Liquidado-> ({persona.ValorLiquidacion})");
+            Console.ReadKey();
         }
     }
 }
